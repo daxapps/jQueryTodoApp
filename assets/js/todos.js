62e489fb12c317b50956ddb9be6de7b78f033b4c
daxapps <daxapps777@gmail.com>
2017-04-05 16:38:48 -0500

    Enable Delete Todos

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -2,3 +2,11 @@
 $("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500,function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
